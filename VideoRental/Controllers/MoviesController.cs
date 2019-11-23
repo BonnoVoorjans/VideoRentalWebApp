9b11a7b712673831141b428e573adad4ef9a3f19
BonnoVoorjans <bcv_neo@hotmail.com>
2019-11-23 19:04:25 +0100

    Initial Project Setup for a standard .NET MVC Web App

---
+++ VideoRental/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRental.Models;
+
+namespace VideoRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Name = "Shrek", Id = 2, RentalPrice = 5.50};
+            return View(movie);
+        }
+    }
+}
