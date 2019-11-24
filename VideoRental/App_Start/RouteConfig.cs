aa7381eb51b84f617d9ed02d6ff8e287b8b7cc56
Bonno Voorjans <bcv_neo@hotmail.com>
2019-11-24 12:00:46 +0100

    Fixed an error caused by microsoft.codedom package (cant find \bin\roslyn\csc.exe). Also added a basic explanation of different ActionResults in the comments of moviesController

--- VideoRental/App_Start/RouteConfig.cs
+++ VideoRental/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace VideoRental
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}/{id}", //Standaard routes bevatten een controller naam, een action (methode van een controller) en een attribuut id. 
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
--- VideoRental/Controllers/MoviesController.cs
+++ VideoRental/Controllers/MoviesController.cs
@@ -14,6 +14,23 @@ namespace VideoRental.Controllers
         {
             Movie movie = new Movie() {Name = "Shrek", Id = 2, RentalPrice = 5.50};
             return View(movie);
+
+            /*Verschillende soorten ActionResults met helper method:
+             * ViewResult - View()
+             * PartialViewResult - PartiealView()
+             * ContentResult - Content()
+             * RedirectResult - Redirect()
+             * RedirectToRouteResult - RedirectToAction() de redirect kan inclusief parameters: RedirectToAction("Index", "Home", new {page = 1, sortBy = "name asc"});
+             * JsonResult - Json()
+             * FileResult - File()
+             * HttpNotFoundResult - HttpNotFound()
+             * EmptyResult (like void) - no helper 
+             */
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id); 
         }
     }
 }
